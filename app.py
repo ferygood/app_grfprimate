de9eceef4f0a9044582d21e6996a54d0f703bac0
ferygood <yaochung41@gmail.com>
2023-05-23 16:40:56 +0200

    add collapse menu

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/alignment')
+def alignment():
+    return render_template('alignment.html')
+
 @app.route('/download')
 def download():
     return render_template('download.html')
