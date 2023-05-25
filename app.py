ee950d4ba8e9eda952706a2159fa7eb3e78b09b6
yaochung41 <yaochung41@gmail.com>
2023-05-25 21:53:10 +0200

    search start fetching from db table

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import psycopg
+from psycopg import sql
 
 app = Flask(__name__)
 
@@ -6,8 +8,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/search')
+@app.route('/search', methods=['GET', 'POST'])
 def search():
+    if request.method == 'POST':
+        gene = request.form['gene_name']
+
+        # establish a connection
+        with psycopg.connect("dbname=tfprimate user=postgres password=yao123") as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    sql.SQL("""
+                    SELECT EnsemblID, GeneName, Hsap 
+                    FROM primateGRFs
+                    WHERE lower(GeneName) = lower(%s)"""), [gene] 
+                )
+
+                gene_data = cur.fetchall()
+                conn.commit()
+    
+        if not gene_data:
+            message = "Sorry. The request of transcription factor is not found in the database."
+            return render_template('search.html', message=message)
+
+        return render_template('search.html', gene_data=gene_data)
+    
     return render_template('search.html')
 
 @app.route('/alignment')
