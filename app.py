427c187a0ceb25f953e2f2e00a2c27a82d8c5cda
yaochung41 <yaochung41@gmail.com>
2023-06-13 11:47:23 +0200

    use ncbi_summary function

--- app.py
+++ app.py
@@ -13,18 +13,21 @@ def index():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     gene_data = []
+    gene_info = None #initialize first to avoid UnboundLocalError
+
 
     if request.method=='POST':
         gene = request.form['gene_name']
-
+    
         #establish a connection
         with psycopg.connect('dbname=tfprimate user=postgres password=yao123') as conn:
             with conn.cursor() as cur:
                 cur.execute(
                     sql.SQL("""
-                    SELECT EnsemblID, GeneName, Hsap
+                    SELECT primateGRFs.EnsemblID, primateGRFs.GeneName, primateGRFs.Hsap, grfid.Entrezid
                     FROM primateGRFs
-                    WHERE lower(GeneName) = lower(%s) OR lower(EnsemblID) = lower(%s)"""), [gene, gene]
+                    JOIN grfid ON primateGRFs.EnsemblID = grfid.EnsemblID
+                    WHERE lower(primateGRFs.GeneName) = lower(%s) OR lower(primateGRFs.EnsemblID) = lower(%s)"""), [gene, gene]
                 )
 
                 gene_data = cur.fetchall()
@@ -33,7 +36,10 @@ def search():
         if not gene_data:
             message="Your request cannot be found in the database. <br>Please check if you type the correct name or id for transcription factors."
             return render_template('search.html', message=message)
-    return render_template('search.html', gene_data=gene_data)
+        
+    gene_info = ncbi_summary(gene_data[0][3])
+
+    return render_template('search.html', gene_data=gene_data, gene_info=gene_info)
 
 @app.route('/alignment')
 def alignment():
@@ -48,6 +54,6 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8070)
-    #app.run(debug=True)
+    #from waitress import serve
+    #serve(app, host="0.0.0.0", port=8070)
+    app.run(debug=True)
