70410d10342dac685c7fd9e2354db7bf4f822303
ferygood <yaochung41@gmail.com>
2023-11-02 18:37:15 +0100

    remove double index page

--- app.py
+++ app.py
@@ -3,35 +3,29 @@ from tfprimate.getInfo import ncbi_summary
 
 app = Flask(__name__)
 
-base_url = "/grfprimate"
-
-@app.route(f'{base_url}/')
-def grfprimate_index():
+@app.route('/')
+def index():
     return render_template('index.html')
 
-@app.route('/search', methods=['GET', 'POST'])
+@app.route('/grfprimate/search', methods=['GET', 'POST'])
 def search():
     gene_data = []
     gene_info = None #initialize first to avoid UnboundLocalError
 
     return render_template('search.html', gene_data=gene_data, gene_info=gene_info)
 
-@app.route(f'{base_url}/alignment')
+@app.route(f'/grfprimate/alignment')
 def alignment():
     return render_template('alignment.html')
 
-@app.route(f'{base_url}/download')
+@app.route(f'/grfprimate/download')
 def download():
     return render_template('download.html')
 
-@app.route(f'{base_url}/about')
+@app.route(f'/grfpriamte/about')
 def about():
     return render_template('about.html')
 
-# separate for landing
-@app.route(f'/')
-def index():
-    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
