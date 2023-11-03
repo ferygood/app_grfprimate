2077a2580b0294edaf0f98220b11565f87309939
ferygood <yaochung41@gmail.com>
2023-11-03 07:33:24 +0100

    add static url

--- app.py
+++ app.py
@@ -2,31 +2,32 @@ from flask import Flask, render_template, request
 from tfprimate.getInfo import ncbi_summary
 
 app = Flask(__name__)
+app.static_url_path = 'static'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/grfprimate/search', methods=['GET', 'POST'])
+@app.route('/search', methods=['GET', 'POST'])
 def search():
     gene_data = []
     gene_info = None #initialize first to avoid UnboundLocalError
 
     return render_template('search.html', gene_data=gene_data, gene_info=gene_info)
 
-@app.route(f'/grfprimate/alignment')
+@app.route('/alignment')
 def alignment():
     return render_template('alignment.html')
 
-@app.route(f'/grfprimate/download')
+@app.route('/download')
 def download():
     return render_template('download.html')
 
-@app.route(f'/grfpriamte/about')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
     
